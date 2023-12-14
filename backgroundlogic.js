409b2578ca45b7035fa3e974263652bb62d00dbd
cupofdirtfordinner <75740323+cupofdirtfordinner@users.noreply.github.com>
2023-12-14 13:32:49 -0600

    Create backgroundlogic.js

---
+++ backgroundlogic.js
@@ -0,0 +1,29 @@
+setInterval(function() {
+  stylish()
+}, 1);
+
+const x = "x"
+const y = "y"
+function Gradients(initRGB, toRGB, distance, dimension) {
+  var pos = 0
+  if (dimension == "x") {
+    pos = xpos
+  } else if (dimension == "y") {
+    pos = ypos
+  }
+  return ""+ ((xpos / -distance)*(toRGB.r-initRGB.r)+initRGB.r) +", "+ ((xpos / -distance)*(toRGB.g-initRGB.g)+initRGB.g) +", "+ ((xpos / -distance)*(toRGB.b-initRGB.b)+initRGB.b)
+}
+
+function stylish() {
+  document.getElementById("canvas").style.background = "radial-gradient(circle, rgb("+Gradients( colors3d[0][0], colors3d[1][0], 350, 'x' )+") 0%, rgb("+Gradients( colors3d[0][1], colors3d[1][1], 300, 'x' )+") 50%, rgb("+Gradients( colors3d[0][2], colors3d[1][2], 450, 'x' )+") 100%)"
+  spans[0].textContent = JSON.stringify(colors3d[0][1])
+}
+
+var colors3d = [
+  [ {r:22,g:234,b:254}, {r:83,g:150,b:255}, {r:18,g:255,b:205} ],
+  [ {r:220,g:23,b:140}, {r:39,g:222,b:12}, {r:90,g:4,b:6} ],
+  [ {}, {}, {} ]
+]
+
+//this system isnt nearly as flexible as i'll need it to be (only 2 colors before things go beyond my defined colors)
+//gotta think of something else soon. this'll do for now.
