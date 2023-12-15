b26fda7fcaadb91dacc88f3139d6684cc00541ce
cupofdirtfordinner <75740323+cupofdirtfordinner@users.noreply.github.com>
2023-12-14 19:59:59 -0800

    Update physics.js

--- physics.js
+++ physics.js
@@ -8,28 +8,29 @@ setInterval(function() {
   phys()
 }, 1);
 
+var direction
+var lastdirection = 0
 var ypos = 0
 var xpos = 0
 var yvel = 0
 var xvel = 0
 var gravity = .00275
+var preypos = 0
+var prexpos = 0
+var preyvel = 0
+var prexvel = 0
 
+var escaper = 0
 var preventspam = false
   
 var jumpheight = 0
 
 function setpres() {
-  var preypos = 0
-  var prexpos = 0
-  var preyvel = 0
-  var prexvel = 0
+  preypos = ypos
+  preyvel = yvel
 }
 
 function phys() {
-  preypos = ypos
-  prexpos = xpos
-  preyvel = yvel
-  prexvel = xvel
   xvel /= 1.2
   yvel /= 1.1
   
@@ -46,24 +47,27 @@ function phys() {
   }
 
   if (pressedKeys[65]) {
-    xvel += speed
-    direction = -1
+    direction = 1
+    xvel += direction * speed
   }
 
   if (pressedKeys[68]) {
-    xvel -= speed
-    direction = 1
+    direction = -1
+    xvel += direction * speed
   }
   
-  if (canjump > 0) {
-    if (pressedKeys[87] ) {
-      gravity = 0
-      jumpheight = 1
-      canjump -= 1
-    } else {
+  if (pressedKeys[87]) {
+    if (canjump > 0) {
+      if (preventspam) {
+        gravity = 0
+        jumpheight = 1
+        canjump -= 1
+        preventspam = false
+      }
+    }
+  } else {
       preventspam = true
     }
-  }
   
   if (pressedKeys[83]) {
       yvel -= speed * 2
@@ -78,28 +82,74 @@ function phys() {
       (rect.top < rectSelection.bottom) &&
       (rect.left < rectSelection.right))
     {
-      CG.style.marginTop = preypos + "vmin" 
+      CG.style.marginTop = preypos + "vmin"
       yvel *= -1
       gravity = 0
-      canjump = 2
+      canjump = 1
     } else {
       causegravity = true
     }
   
   }
-if (causegravity) {
-  gravity += .00015
-  gravity *= (9.81 * 0.103)
-  yvel -= gravity
-  CG.style.marginTop = ypos + "vmin"
-}
-
+  rectSelection = document.getElementById("wallCollider").getBoundingClientRect();
+  for (let i = 0; i < document.querySelectorAll(".mainWall").length; i++) {
+    var rect = document.querySelectorAll(".mainWall")[i].getBoundingClientRect();
+    if ((rect.bottom > rectSelection.top) &&
+      (rect.right > rectSelection.left) &&
+      (rect.top < rectSelection.bottom) &&
+      (rect.left < rectSelection.right))
+    {
+      CG.style.marginLeft = prexpos + "vmin"
+      wallcollisions = true
+      
+    } else {
+      wallcollisions = false
+    }
+  
+  }
+  rectSelection = document.getElementById("wallCollider2").getBoundingClientRect();
+  for (let i = 0; i < document.querySelectorAll(".mainWall").length; i++) {
+    var rect = document.querySelectorAll(".mainWall")[i].getBoundingClientRect();
+    if ((rect.bottom > rectSelection.top) &&
+      (rect.right > rectSelection.left) &&
+      (rect.top < rectSelection.bottom) &&
+      (rect.left < rectSelection.right))
+    {
+      wallcollisions2 = true
+      
+    } else {
+      wallcollisions2 = false
+      lastdirection = direction
+    }
+  
+  }
+  
+  if (wallcollisions) {
+    xvel = prexvel * -.1
+  } else {
+    prexvel = xvel
+  }
+  if (wallcollisions2) {
+    gravity = .01
+  }
+  
+  
+  if (causegravity) {
+    gravity += .00015
+    gravity *= (9.81 * 0.103)
+    yvel -= gravity
+    CG.style.marginTop = ypos + "vmin"
+  }
   
   CG.style.marginLeft = xpos + "vmin"
       
   pm.style.marginTop = "calc(57vmin - "+ypos+"vmin)"
   pm.style.marginLeft = "calc(48.5vmin - "+xpos+"vmin)"
   
+  document.querySelector("#grounds").style.perspectiveOrigin = "calc(48.5vmin - "+xpos+"vmin) calc(20vmin - "+ypos+"vmin)"
+  
   canvas.style.left = "calc(50% - "+ (canvas.getBoundingClientRect().right - canvas.getBoundingClientRect().left)/2 +"px)"
+
+  spans[0].textContent = wallcollisions2
 }
 
