5bc328d5892c2939246a11672da7bc35e0f7da7d
cupofdirtfordinner <cupofdirtfordinner@gmail.com>
2023-12-15 22:03:01 -0600

    poopoopeepeeopoopoopeeeee

--- physics.js
+++ physics.js
@@ -8,22 +8,53 @@ setInterval(function() {
   phys()
 }, 1);
 
-var direction
+var variable = 0
+var wallcollisions = false
+var wallCollisions2 = false
+var direction = 0
 var lastdirection = 0
 var ypos = 0
 var xpos = 0
 var yvel = 0
 var xvel = 0
-var gravity = .00275
+var gravity = 0
 var preypos = 0
 var prexpos = 0
 var preyvel = 0
 var prexvel = 0
-
+var causegravity = false
 var escaper = 0
 var preventspam = false
-  
+var preventspam2 = false
+var dash = 0
 var jumpheight = 0
+
+function collision(element) {
+  rectSelection = document.getElementById(element).getBoundingClientRect();
+  var GoodToGo = 0
+  for (let i = 0; i < document.querySelectorAll(".mainWall").length; i++) {
+    var rect = document.querySelectorAll(".mainWall")[i].getBoundingClientRect();
+    if (element == "wallCollider") {
+        
+        variable++
+        spans[1].textContent = rect.bottom > rectSelection.top
+        spans[2].textContent = rect.right > rectSelection.left
+        spans[3].textContent = rect.top < rectSelection.bottom
+        spans[4].textContent = rect.left < rectSelection.right
+      }
+    if ((rect.bottom > rectSelection.top) &&
+      (rect.right > rectSelection.left) &&
+      (rect.top < rectSelection.bottom) &&
+      (rect.left < rectSelection.right))
+    {
+      GoodToGo++
+    } else {
+      
+    }
+  }
+  return GoodToGo
+}
+
 
 function setpres() {
   preypos = ypos
@@ -31,12 +62,14 @@ function setpres() {
 }
 
 function phys() {
+  if (collision("wallCollider")>-1){}
   xvel /= 1.2
   yvel /= 1.1
+  dash /= 1.05
   
   jumpheight /= 1.05
   ypos += (yvel + jumpheight)
-  xpos += xvel
+  xpos += xvel + (dash * direction)
   
   //controls
   window.onkeyup = function(e) {
@@ -60,7 +93,7 @@ function phys() {
     if (canjump > 0) {
       if (preventspam) {
         gravity = 0
-        jumpheight = 1
+        jumpheight = 1.4
         canjump -= 1
         preventspam = false
       }
@@ -71,66 +104,45 @@ function phys() {
   
   if (pressedKeys[83]) {
       yvel -= speed * 2
+      if (wallCollisions2) {
+        gravity = 1
+      }
   }
+  if (pressedKeys[16]) {
+    if (dash < .1) {
+      //if (preventspam2) {
+        gravity = -.005
+        dash = 2
+        preventspam2 = false
+      //}
+    }
+  } else {
+      preventspam2 = true
+    }
+    
   
-  var causegravity = false
-  var rectSelection = document.getElementById("floorCollider").getBoundingClientRect();
-  for (let i = 0; i < document.querySelectorAll(".mainWall").length; i++) {
-    var rect = document.querySelectorAll(".mainWall")[i].getBoundingClientRect();
-    if ((rect.bottom > rectSelection.top) &&
-      (rect.right > rectSelection.left) &&
-      (rect.top < rectSelection.bottom) &&
-      (rect.left < rectSelection.right))
-    {
-      CG.style.marginTop = preypos + "vmin"
-      yvel *= -1
-      gravity = 0
-      canjump = 1
-    } else {
+  
+  if (collision("floorCollider")>0) {
+   CG.style.marginTop = preypos + "vmin"
+      yvel *= -1 //jumpheight = 2
+      gravity = -.001
       causegravity = true
-    }
-  
-  }
-  rectSelection = document.getElementById("wallCollider").getBoundingClientRect();
-  for (let i = 0; i < document.querySelectorAll(".mainWall").length; i++) {
-    var rect = document.querySelectorAll(".mainWall")[i].getBoundingClientRect();
-    if ((rect.bottom > rectSelection.top) &&
-      (rect.right > rectSelection.left) &&
-      (rect.top < rectSelection.bottom) &&
-      (rect.left < rectSelection.right))
-    {
-      CG.style.marginLeft = prexpos + "vmin"
-      wallcollisions = true
-      
-    } else {
-      wallcollisions = false
-    }
-  
-  }
-  rectSelection = document.getElementById("wallCollider2").getBoundingClientRect();
-  for (let i = 0; i < document.querySelectorAll(".mainWall").length; i++) {
-    var rect = document.querySelectorAll(".mainWall")[i].getBoundingClientRect();
-    if ((rect.bottom > rectSelection.top) &&
-      (rect.right > rectSelection.left) &&
-      (rect.top < rectSelection.bottom) &&
-      (rect.left < rectSelection.right))
-    {
-      wallcollisions2 = true
-      
-    } else {
-      wallcollisions2 = false
-      lastdirection = direction
-    }
-  
+      canjump = 1
+  } else {
+      causegravity = true
   }
   
-  if (wallcollisions) {
+  if (collision("wallCollider")>0) {
     xvel = prexvel * -.1
   } else {
     prexvel = xvel
   }
-  if (wallcollisions2) {
+  spans[0].textContent = variable
+  
+  if (collision("wallCollider2")) {
     gravity = .01
+  } else {
+    lastdirection = direction
   }
   
   
@@ -143,13 +155,11 @@ function phys() {
   
   CG.style.marginLeft = xpos + "vmin"
       
-  pm.style.marginTop = "calc(57vmin - "+ypos+"vmin)"
+  pm.style.marginTop = "calc(60vmin - "+ypos+"vmin)"
   pm.style.marginLeft = "calc(48.5vmin - "+xpos+"vmin)"
   
   document.querySelector("#grounds").style.perspectiveOrigin = "calc(48.5vmin - "+xpos+"vmin) calc(20vmin - "+ypos+"vmin)"
   
   canvas.style.left = "calc(50% - "+ (canvas.getBoundingClientRect().right - canvas.getBoundingClientRect().left)/2 +"px)"
-
-  spans[0].textContent = wallcollisions2
 }
 
--- worldbuilder.js
+++ worldbuilder.js
@@ -7,27 +7,39 @@ var depth = 50
 var pressedKeys = {};
 
 //adding player collision boxes
+
 const floorCollider = document.createElement("div")
 floorCollider.style.width = "100%"
-floorCollider.style.height = "5px"
+floorCollider.style.height = ".01vmin"
 floorCollider.style.marginTop = "100%"
-floorCollider.style.backgroundColor = "rgba(255, 255, 0, 1)"
+//floorCollider.style.backgroundColor = "rgba(255, 255, 0, 1)"
 floorCollider.setAttribute("id", "floorCollider")
 player.appendChild(floorCollider)
+
+const floorCollideXtra = document.createElement("div")
+floorCollideXtra.style.width = "100%"
+floorCollideXtra.style.position = "absolute"
+floorCollideXtra.style.height = ".01vmin"
+floorCollideXtra.style.top = "calc(100% + .01vmin)"
+//floorCollideXtra.style.backgroundColor = "rgba(25, 255, 0, 1)"
+floorCollideXtra.setAttribute("id", "floorCollideXtra")
+floorCollider.appendChild(floorCollideXtra)
+
 const wallCollider = document.createElement("div")
 wallCollider.style.width = "calc(100% - .5vmin)"
 wallCollider.style.height = "calc(100% - 1vmin)"
 wallCollider.style.marginLeft = ".25vmin"
 wallCollider.style.marginTop = "-6vmin"
-wallCollider.style.backgroundColor = "rgba(255, 255, 255, 1)"
+//wallCollider.style.backgroundColor = "rgba(255, 255, 255, 1)"
 wallCollider.setAttribute("id", "wallCollider")
 player.appendChild(wallCollider)
+
 const wallCollider2 = document.createElement("div")
 wallCollider2.style.width = "calc(100% + .5vmin)"
 wallCollider2.style.height = "calc(100% - 1vmin)"
 wallCollider2.style.marginLeft = "-.25vmin"
 wallCollider2.style.marginTop = "-6vmin"
-wallCollider2.style.backgroundColor = "rgba(0, 255, 255, 1)"
+//wallCollider2.style.backgroundColor = "rgba(0, 255, 255, 1)"
 wallCollider2.setAttribute("id", "wallCollider2")
 player.appendChild(wallCollider2)
 
@@ -59,8 +71,24 @@ function plane(width, height, x, y) {
   bottom.style.left = x + "vmin"
   bottom.style.top = "calc("+y+"vmin + "+height+"vmin - "+70/2+"px)"
   bottom.classList.add("bottomWall")
+  
+  left.style.position = "absolute"
+  left.style.width = "70px"
+  left.style.height = height + "vmin"
+  left.style.left = x + "vmin"
+  left.style.top = +y+"vmin"
+  left.classList.add("leftWall")
+  
+  right.style.position = "absolute"
+  right.style.width = "70px"
+  right.style.height = height + "vmin"
+  right.style.left = x+width + "vmin"
+  right.style.top = +y+"vmin"
+  right.classList.add("rightWall")
 
   CG.appendChild(main)
   CG.appendChild(top)
   CG.appendChild(bottom)
+  CG.appendChild(left)
+  CG.appendChild(right)
 }
