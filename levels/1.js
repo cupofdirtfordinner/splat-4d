94a5998528507fef782132a8c9baf791492a63bd
cupofdirtfordinner <75740323+cupofdirtfordinner@users.noreply.github.com>
2024-01-17 12:56:39 -0600

    Update 1.js

--- levels/1.js
+++ levels/1.js
@@ -1,7 +1,9 @@
-//size x  y | x  y
+//size x  y | x  y | is-a-background boolean
 plane(500, 20, 2, 92)
 plane(20, 120, 2, -20)
 plane(24, 70, 197, 60)
 plane(35, 70, 137, 60)
 plane(30, 30, 55, 20)
-
+plane(50, 20, 40, 92, true)
+plane(30, 40, 100, 52, true)
+spikes(25, 20, 170, 70)
